12d1427a8ccd0009d689bf5db899863771d01b84
NguyenQuocTien <nguyenquoctien2401@gmail.com>
2024-10-11 22:42:55 +0700

    day10-api-server

--- server/src/app/controllers/RegisterController.js
+++ server/src/app/controllers/RegisterController.js
@@ -1,13 +1,18 @@
-
+import User from '../models/User.js';
 class RegisterController {
-    // Method Get locallhost/register:
-    getRegister(req, res) {
-        res.send('Get request for register page');
-    }
-    // Method Post
-    register(req, res) {
-        console.log(req.body);
-        res.send('Post request for register page');
+    // POST hocalhost/register
+    async register(req, res) {
+        try {
+            const userExists = await User.findByUsername(req.body);
+            if (userExists) {
+                return res.status(400).json({ error: 'User already exists.' });
+            }
+            const newUserId = await User.create(req.body);
+            return res.status(201).json({ id: newUserId });
+        } catch (error) {
+            console.error('Error during registration:', error); // Log lỗi chi tiết
+            return res.status(500).json({ error: error.message });
+        }
     }
 }
 
--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -3,7 +3,6 @@ import LoginController from '../app/controllers/LoginController.js'
 
 const router = express.Router();
 
-router.get('/', LoginController.getLogin);
 router.post('/', LoginController.login);
 
 export default router;
